7758a67ee607d64845db8b97b1321b2a08c39be9
nikamni333 <nikamni333@gmail.com>
2023-11-25 02:47:47 +0530

    All

--- sv/src/main/java/com/nn/sv/repository/TripRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nn.sv.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.nn.sv.model.TripEntity;
-
-public interface TripRepository extends JpaRepository<TripEntity, Long>{
-
-}
